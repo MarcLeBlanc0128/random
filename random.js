ab7a946434c8b49bd46d91fc7c5dfe95a9e0c026
Marc LeBlanc <marcleblancii0128@gmail.com>
2017-02-09 13:32:55 -0800

    finished

--- random.js
+++ random.js
@@ -5,7 +5,57 @@ var namespace = "http://www.w3.org/2000/svg"
 // You need to use at least 3 different types of shape.
 // Remember, for the shapes to show up on the canvas, you'll need to CALL the function.
 function createFirstScene() {
-    
+    makeRect(0,0,500,500,"cornflowerblue", 1)
+
+makeRect(50, 70, 50, 25, "blue", 1)
+makeRect(70, 60, 50, 25, "blue", 1)
+makeRect(110, 54, 20, 31, "saddlebrown", 1)
+makeRect(30, 75, 20, 20, "saddlebrown", 1)
+makeRect(20, 85, 20, 20, "saddlebrown", 1)
+makeRect(40, 50, 30, 20, "red", 1)
+makeRect(20, 45, 80, 15, "red", 1)
+makeRect(70, 53, 8, 8, "blue", 1)
+makeRect(63, 45, 8, 8, "blue", 1)
+makeRect(92, 53, 8, 8, "blue", 1)
+makeRect(84, 45, 8, 8, "blue", 1)
+makeRect(99, 45, 8, 8, "red", 1)
+makeRect(99, 37, 8, 8, "red", 1)
+makeRect(107, 37, 8, 8, "red", 1)
+makeRect(107, 30, 8, 8, "red", 1)
+makeRect(92, 60, 8, 8, "yellow", 1)
+makeRect(78, 70, 8, 8, "yellow", 1)
+makeRect(49, 31, 55, 14, "navajowhite", 1)
+makeRect(49, 15, 50, 14, "navajowhite", 1)
+
+makeRect(49, 9, 45, 30, "navajowhite", 1)
+makeRect(80,30, 27, 7, "black", 1)
+makeRect(86, 23, 8, 8, "black", 1)
+makeRect(78, 16, 8, 8, "black", 1)
+makeRect(78, 9, 8, 8, "black", 1)
+makeRect(98, 22, 8, 8, "navajowhite", 1)
+makeRect(98, 15, 8, 8, "navajowhite", 1)
+makeRect(106, 22, 9, 8, "navajowhite", 1)
+makeRect(49, 9, 7, 23, "saddlebrown", 1)
+makeRect(55, 25, 7, 7, "saddlebrown", 1)
+makeRect(55, 9, 7, 7, "saddlebrown", 1)
+makeRect(42, 9, 8, 7, "saddlebrown", 1)
+makeRect(42, 16, 7, 15, "navajowhite", 1)
+makeRect(35, 16, 7, 21, "saddlebrown", 1)
+makeRect(41, 30, 8, 7, "saddlebrown", 1)
+makeRect(49, -2, 40, 14, "red", 1)
+makeRect(69, 0, 20, 7, "red", 1)
+makeRect(10, 44.5, 20, 20, "navajowhite", 1)
+makeRect(115, 22, 9, 9, "red", 1)
+makeRect(106, 7.5, 18, 15, "red", 1)
+makeRect(106, 0, 20, 20, "navajowhite", 1)
+makeCircle(400, 50, 25, "white", 1)
+
+makeRect(197,5.9, 6, 501, "white", 1)
+makePolygon("203,10 300,60 203,60", "black", 1)
+makeCircle(200, 10, 10, "gold", 1)
+
+
+
 }
 
 
@@ -13,15 +63,230 @@ function createFirstScene() {
 // You need to use at least 3 different types of shape.
 // Remember, for the shapes to show up on the canvas, you'll need to CALL the function.
 function createSecondScene() {
-    
+    makeRect(0,0,500,500,"cornflowerblue", 1)
+
+makeRect(50, 70, 50, 25, "blue", 1)
+makeRect(70, 60, 50, 25, "blue", 1)
+makeRect(110, 54, 20, 31, "saddlebrown", 1)
+makeRect(30, 75, 20, 20, "saddlebrown", 1)
+makeRect(20, 85, 20, 20, "saddlebrown", 1)
+makeRect(40, 50, 30, 20, "green", 1)
+makeRect(20, 45, 80, 15, "green", 1)
+makeRect(70, 53, 8, 8, "blue", 1)
+makeRect(63, 45, 8, 8, "blue", 1)
+makeRect(92, 53, 8, 8, "blue", 1)
+makeRect(84, 45, 8, 8, "blue", 1)
+makeRect(99, 45, 8, 8, "green", 1)
+makeRect(99, 37, 8, 8, "green", 1)
+makeRect(107, 37, 8, 8, "green", 1)
+makeRect(107, 30, 8, 8, "green", 1)
+makeRect(92, 60, 8, 8, "yellow", 1)
+makeRect(78, 70, 8, 8, "yellow", 1)
+makeRect(49, 31, 55, 14, "navajowhite", 1)
+makeRect(49, 15, 50, 14, "navajowhite", 1)
+
+makeRect(49, 9, 45, 30, "navajowhite", 1)
+makeRect(80,30, 27, 7, "black", 1)
+makeRect(86, 23, 8, 8, "black", 1)
+makeRect(78, 16, 8, 8, "black", 1)
+makeRect(78, 9, 8, 8, "black", 1)
+makeRect(98, 22, 8, 8, "navajowhite", 1)
+makeRect(98, 15, 8, 8, "navajowhite", 1)
+makeRect(106, 22, 9, 8, "navajowhite", 1)
+makeRect(49, 9, 7, 23, "saddlebrown", 1)
+makeRect(55, 25, 7, 7, "saddlebrown", 1)
+makeRect(55, 9, 7, 7, "saddlebrown", 1)
+makeRect(42, 9, 8, 7, "saddlebrown", 1)
+makeRect(42, 16, 7, 15, "navajowhite", 1)
+makeRect(35, 16, 7, 21, "saddlebrown", 1)
+makeRect(41, 30, 8, 7, "saddlebrown", 1)
+makeRect(49, -2, 40, 14, "green", 1)
+makeRect(69, 0, 20, 7, "green", 1)
+makeRect(10, 44.5, 20, 20, "navajowhite", 1)
+makeRect(115, 22, 9, 9, "green", 1)
+makeRect(106, 7.5, 18, 15, "green", 1)
+makeRect(106, 0, 20, 20, "navajowhite", 1)
+
+makeRect(197,5.9, 6, 501, "white", 1)
+makePolygon("203,10 300,60 203,60", "black", 1)
+makeCircle(200, 10, 10, "gold", 1)
 }
+
+
+
+
+
+// DO NOT EDIT ANYTHING BELOW THIS LINE!
+// These are the functions you should call to
+// easily create shapes in JavaScript. Feel free
+// to check them out if you're curious how they
+// work, but don't change them!
+// <3 Ms. Squires
+function makeCircle(cx, cy, r, fill, opacity) {
+  var circle = document.createElementNS(namespace, "circle")
+  circle.setAttribute("cx", cx)
+  circle.setAttribute("cy", cy)
+  circle.setAttribute("r", r)
+  circle.setAttribute("fill", fill)
+  circle.setAttribute("opacity", opacity)
+  
+  var canvas = document.getElementById("canvas")
+  canvas.appendChild(circle)
+  return circle
+}
+
+function makeRect(x, y, width, height, fill, opacity) {
+  var rect = document.createElementNS(namespace, "rect")
+  rect.setAttribute("x", x)
+  rect.setAttribute("y", y)
+  rect.setAttribute("width", width)
+  rect.setAttribute("height", height)
+  rect.setAttribute("fill", fill)
+  rect.setAttribute("opacity", opacity)
+  
+  var canvas = document.getElementById("canvas")
+  canvas.appendChild(rect)
+  return rect
+}
+
+function makeEllipse(cx, cy, rx, ry, fill, opacity) {
+  var ellipse = document.createElementNS(namespace, "ellipse")
+  ellipse.setAttribute("cx", cx)
+  ellipse.setAttribute("cy", cy)
+  ellipse.setAttribute("rx", rx)
+  ellipse.setAttribute("ry", ry)
+  ellipse.setAttribute("fill", fill)
+  ellipse.setAttribute("opacity", opacity)
+  
+  var canvas = document.getElementById("canvas")
+  canvas.appendChild(ellipse)
+  return ellipse
+}
+
+function makeLine(x1, y1, x2, y2, stroke, strokeWidth, opacity) {
+  var line = document.createElementNS(namespace, "line")
+  line.setAttribute("x1", x1)
+  line.setAttribute("y1", y1)
+  line.setAttribute("x2", x2)
+  line.setAttribute("y2", y2)
+  line.setAttribute("stroke", stroke)
+  line.setAttribute("stroke-width", strokeWidth)
+  line.setAttribute("opacity", opacity)
+  
+  var canvas = document.getElementById("canvas")
+  canvas.appendChild(line)
+  return line
+}
+
+function makePolyline(points, stroke, strokeWidth, opacity) {
+  var polyline = document.createElementNS(namespace, "polyline")
+  polyline.setAttribute("points", points)
+  polyline.setAttribute("stroke", stroke)
+  polyline.setAttribute("stroke-width", strokeWidth)
+  polyline.setAttribute("opacity", opacity)
+  polyline.setAttribute("fill", "none")
+  
+  var canvas = document.getElementById("canvas")
+  canvas.appendChild(polyline)
+  return polyline
+}
+
+function makePolygon(points, fill, opacity) {
+  var polygon = document.createElementNS(namespace, "polygon")
+  polygon.setAttribute("points", points)
+  polygon.setAttribute("opacity", opacity)
+  polygon.setAttribute("fill", fill)
+  
+  var canvas = document.getElementById("canvas")
+  canvas.appendChild(polygon)
+  return polygon
+}
+
+function makeText(message, x, y, fontSize, fontFamily, fill, opacity) {
+  var text = document.createElementNS(namespace, "text")
+  text.innerHTML = message
+  text.setAttribute("x", x)
+  text.setAttribute("y", y)
+  text.setAttribute("font-size", fontSize)
+  text.setAttribute("font-family", fontFamily)
+  text.setAttribute("fill", fill)
+  text.setAttribute("opacity", opacity)
+  
+  var canvas = document.getElementById("canvas")
+  canvas.appendChild(text)
+  return text
+}
+
+function makeImage(url, x, y, width, height, opacity) {
+  var image = document.createElementNS(namespace, "image")
+  image.setAttributeNS("http://www.w3.org/1999/xlink", "xlink:href", url)
+  image.setAttribute("x", x)
+  image.setAttribute("y", y)
+  image.setAttribute("width", width)
+  image.setAttribute("height", height)
+  image.setAttribute("opacity", opacity)
+  
+  var canvas = document.getElementById("canvas")
+  canvas.appendChild(image)
+  return image
+}
+
 
 
 // Fill in this function so that it draws something using SVG shapes!
 // You need to use at least 3 different types of shape.
 // Remember, for the shapes to show up on the canvas, you'll need to CALL the function.
 function createThirdScene() {
-    
+    makeRect(0,0,500,500,"cornflowerblue", 1)
+
+    makeRect(110, 0, 37, 6, "black", 1)
+makeRect(93, 5.5, 17, 6, "black", 1)
+makeRect(147, 5.5, 17, 6, "black", 1)
+makeRect(158,10.7, 6, 6, "black", 1)
+makeRect(93,10.7, 6, 6, "black", 1)
+makeRect(87,10.7, 6.5, 6, "black", 1)
+makeRect(163.5,10.7, 6.5, 6, "black", 1)
+makeRect(163.5,16.6, 6, 6, "black", 1)
+makeRect(86.5,16.6, 6.5, 6, "black", 1)
+makeRect(81,16.6, 6, 6, "black", 1)
+makeRect(169,16.6, 6, 6, "black", 1)
+makeRect(169,16.6, 6, 17, "black", 1)
+makeRect(80,16.6, 6, 17, "black", 1)
+makeRect(73,27.6, 13, 6, "black", 1)
+makeRect(169,27.6, 13, 6, "black", 1)
+makeRect(176,27.6, 6, 46, "black", 1)
+makeRect(72,27.6, 6, 46, "black", 1)
+makeRect(72,70.6, 33, 6, "black", 1)
+makeRect(149,70.6, 33, 6, "black", 1)
+makeRect(105,64.7, 44, 6, "black", 1)
+makeRect(72,70.6, 29, 6, "black", 1)
+makeRect(83,76.4, 6, 6, "black", 1)
+makeRect(165,76.4, 6, 6, "black", 1)
+makeRect(159.5,82.4, 6, 18, "black", 1)
+makeRect(88.5,82.4, 6, 18, "black", 1)
+makeRect(88.5,95.4, 12, 6, "black", 1)
+makeRect(153.5,95.4, 12, 6, "black", 1)
+makeRect(150.5,95.4, 6, 12, "black", 1)
+makeRect(94.6,95.4, 6, 12, "black", 1)
+makeRect(100.5,101.4, 51, 6, "black", 1)
+makeRect(112.6,70.4, 6, 12, "black", 1)
+makeRect(135.6,70.4, 6, 12, "black", 1)
+makeRect(110.6,31, 32, 34, "red", 1)
+makeRect(105.6,36, 42, 24, "red", 1)
+makeRect(77.6,33, 17, 25, "red", 1)
+makeRect(77.6,33, 22, 17, "red", 1)
+makeRect(84.6,22, 8.5, 12, "red", 1)
+makeRect(159,33.4, 17, 25, "red", 1)
+makeRect(153.6,33, 22, 17, "red", 1)
+makeRect(160.6,22, 8.5, 12, "red", 1)
+makeRect(147,11.4, 11, 11, "red", 1)
+makeRect(99,11.4, 11, 11, "red", 1)
+makeRect(110,5.9, 6, 11, "red", 1)
+makeRect(141,5.9, 6, 11, "red", 1)
+
+makeRect(197,5.9, 6, 501, "white", 1)
+makePolygon("203,10 300,60 203,60", "black", 1)
+makeCircle(200, 10, 10, "gold", 1)
 }
 
 
@@ -32,17 +297,23 @@ function createThirdScene() {
 // This function is called whenever you press the "Go!" button.
 function createRandomScene() {
     // Generate a random number between 0 and 1, and store it in a variable.
-    
+    var rando = Math.random()
     // If the number is less than 0.33, call the function to create your first scene.
-    
+    if (rando < 0.33){
+    createFirstScene()
+    }
     
     
     // Else, if the number is less than 0.67, call the function to create your second scene.
-    
+    else if (rando < 0.67){
+    createSecondScene()
+    }
     
     
     // Else, call the function to create your third scene.
-    
+    else{
+    createThirdScene()
+    }
     
     
 }
